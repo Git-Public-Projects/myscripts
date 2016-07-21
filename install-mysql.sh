832cec27dcc819d8ab58aaebbfd590a182b2c4cf
Arijit Bardhan <arijit.bardhan@mindtree.com>
2016-07-21 13:28:32 +0530

    Update install-mysql.sh

--- install-mysql.sh
+++ install-mysql.sh
@@ -3,8 +3,6 @@ apt-get update
 
 # set up a silent install of MySQL
 
-#sudo mkdir -p /opt/dbscript
-#cd /opt/dbscript && wget https://adidaswarstorage.blob.core.windows.net/sqlfileupload/myarenadb_script.sql
 export DEBIAN_FRONTEND=noninteractive
 echo mysql-server-5.6 mysql-server/root_password password welcome123 | debconf-set-selections
 echo mysql-server-5.6 mysql-server/root_password_again password welcome123 | debconf-set-selections
@@ -16,6 +14,5 @@ sed -i -e"s/^bind-address\s*=\s*127.0.0.1/bind-address = 0.0.0.0/" /etc/mysql/my
 
 sudo service mysql restart
 echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'welcome123'; flush privileges;" | mysql -u root -pwelcome123
-#mysql -u root -pwelcome123 < /opt/dbscript/myarenadb_script.sql
 
 #echo "CREATE DATABASE sonar CHARACTER SET utf8 COLLATE utf8_general_ci; CREATE USER 'sonar' IDENTIFIED BY 'sonar';GRANT ALL PRIVILEGES ON sonar.* TO 'sonar'@'%' IDENTIFIED BY 'sonar'; GRANT ALL ON sonar.* TO 'sonar'@'localhost' IDENTIFIED BY 'sonar'; flush privileges;" | mysql -u root -pwelcome123
